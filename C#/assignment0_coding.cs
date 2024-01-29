fd7a669ca84fb2e03e07bd062089a92a00dc2443
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-29 09:47:42 -0600

    Create assignment0_coding.cs

---
+++ C#/assignment0_coding.cs
@@ -0,0 +1,16 @@
+/*
+Project Name: assignment0_coding
+Name: Tristen Stegall
+Date: 1/24/2024
+Description: This program implements the algorithm from Lab 0 Part 1 Logic. It prints name, address, and Postal Info.
+Language: C#
+Editor: OnlineGDB
+*/
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Tristen Stegall");
+    Console.WriteLine("1234 Walnut Street");
+    Console.WriteLine("Asheville, NC 28899");
+  }
+}
