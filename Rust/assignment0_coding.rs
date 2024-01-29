370043625771acd25b457e25b0a1070093405525
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-29 09:48:23 -0600

    Create assignment0_coding.rs

---
+++ Rust/assignment0_coding.rs
@@ -0,0 +1,13 @@
+/*
+Project Name: assignment0_coding
+Name: Tristen Stegall
+Date: 1/24/2024
+Description: This program implements the algorithm from Lab 0 Part 1 Logic. It prints name, address, and Postal Info.
+Language: Rust
+Editor: OnlineGDB
+*/
+fn main() {
+    println!("Tristen Stegall");
+    println!("1234 Walnut Street");
+    println!("Asheville, NC 28899");
+}
