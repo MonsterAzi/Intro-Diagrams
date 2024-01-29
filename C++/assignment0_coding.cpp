b33801045ea96622e02b4711b94f5a066b340e82
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-29 09:44:53 -0600

    Create assignment0_coding.cpp

---
+++ C++/assignment0_coding.cpp
@@ -0,0 +1,17 @@
+/*
+Project Name: assignment0_coding
+Name: Tristen Stegall
+Date: 1/24/2024
+Description: This program implements the algorithm from Lab 0 Part 1 Logic. It prints name, address, and Postal Info.
+Language: C++ 20
+Editor: OnlineGDB
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "Tristen Stegall\n";
+cout << "1234 Walnut Street\n";
+cout << "Asheville, NC 28899";
+}
