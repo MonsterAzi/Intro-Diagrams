50cd906bb86c22bc4cb8fb9f0e564135076e3bf1
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-29 09:52:44 -0600

    Create assignment0_coding.c

---
+++ C/assignment0_coding.c
@@ -0,0 +1,17 @@
+/*
+Project Name: assignment0_coding
+Name: Tristen Stegall
+Date: 1/24/2024
+Description: This program implements the algorithm from Lab 0 Part 1 Logic. It prints name, address, and Postal Info.
+Language: C
+Editor: OnlineGDB
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Tristen Stegall\n");
+    printf("1234 Walnut Street\n");
+    printf("Asheville, NC 28899");
+}
