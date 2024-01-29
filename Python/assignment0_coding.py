8a44f01a84ea7dad371438002a5cfaf66f6513b0
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-29 09:42:34 -0600

    Create assignment0_coding.py

---
+++ Python/assignment0_coding.py
@@ -0,0 +1,11 @@
+'''
+Project Name: assignment0_coding
+Name: Tristen Stegall
+Date: 1/24/2024
+Description: This program implements the algorithm from Lab 0 Part 1 Logic. It prints name, address, and Postal Info.
+Language: Python
+Editor: OnlineGDB
+'''
+print("Tristen Stegall")
+print("1234 Walnut Street")
+print("Asheville, NC 28899")
