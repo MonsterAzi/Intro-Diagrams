20e79a80c036bf767ee5a413b12d1df7aab8516e
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-29 09:46:39 -0600

    Create assignment0_coding.java

---
+++ Java/assignment0_coding.java
@@ -0,0 +1,16 @@
+/*
+Project Name: assignment0_coding
+Name: Tristen Stegall
+Date: 1/24/2024
+Description: This program implements the algorithm from Lab 0 Part 1 Logic. It prints name, address, and Postal Info.
+Language: Java
+Editor: OnlineGDB
+*/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Tristen Stegall");
+		System.out.println("1234 Walnut Street");
+		System.out.println("Asheville, NC 28899");
+	}
+}
