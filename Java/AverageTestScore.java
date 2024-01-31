26e4e7f55671991112d2400c6dd8f6421d2d0efb
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-31 09:37:13 -0600

    Update AverageTestScore.java

--- Java/AverageTestScore.java
+++ Java/AverageTestScore.java
@@ -3,7 +3,7 @@ Project Name: AverageTestScore
 Name: Tristen Stegall
 Date: 1/31/2024
 Description: Program 2-13, page 59 - The Average Test Score
-Language: Rust
+Language: Java
 Editor: OnlineGDB
 */
 public class Main
