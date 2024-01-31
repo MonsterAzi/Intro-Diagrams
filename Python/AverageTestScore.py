3f9b97c32e85ed1ae2907e6acc4971c8c3abd18f
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-31 09:17:04 -0600

    Create AverageTestScore.py

---
+++ Python/AverageTestScore.py
@@ -0,0 +1,15 @@
+'''
+Project Name: AverageTestScore
+Name: Tristen Stegall
+Date: 1/31/2024
+Description: Program 2-13, page 59 - The Average Test Score
+Language: Python
+Editor: OnlineGDB
+'''
+test1 = 88.0
+test2 = 92.5
+test3 = 97.0
+average: float
+
+average = (test1 + test2 + test3) / 3
+print("Your average test score is", average)
