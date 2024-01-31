17c6f7e92d7f8227de30a2ff2d5d578174b86363
Azazelle Guice <120850865+MonsterAzi@users.noreply.github.com>
2024-01-31 09:23:05 -0600

    Create AverageTestScore.rs

---
+++ Rust/AverageTestScore.rs
@@ -0,0 +1,17 @@
+/*
+Project Name: AverageTestScore
+Name: Tristen Stegall
+Date: 1/31/2024
+Description: Program 2-13, page 59 - The Average Test Score
+Language: Rust
+Editor: OnlineGDB
+*/
+fn main() {
+    let test1 = 88.0;
+    let test2 = 92.5;
+    let test3 = 97.0;
+    let average: f32;
+    
+    average = (test1 + test2 + test3) / 3.0;
+    println!("Your average test score is {average}");
+}
